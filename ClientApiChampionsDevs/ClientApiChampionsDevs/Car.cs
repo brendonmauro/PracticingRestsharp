bc5baa912e2ecf8307117334f73617c1b9010334
MASTER <brendonmauro@hotmail.com>
2024-04-26 22:45:26 -0300

    Feat: Car client created

---
+++ ClientApiChampionsDevs/ClientApiChampionsDevs/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApiChampionsDevs
+{
+    internal class Car
+    {
+        public Car(string name, int year)
+        {
+            Name = name;
+            Year = year;
+        }
+
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Year}";
+        }
+    }
+}
